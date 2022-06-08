6b0a56133cf984f47706dece450b39f1bc2eed3b
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-08 05:06:57 +0500

    Update app.ts

--- third party js library/src/app.ts
+++ third party js library/src/app.ts
@@ -1,3 +1,10 @@
 import _ from 'lodash';
+import { Product } from './product';
+
+declare const GLOBAL: string;
+const product = new Product('Book', 12.99);
 
 console.log(_.shuffle([0, 1, 2, 3]));
+console.log(GLOBAL);
+
+console.log(product.getInfo());
